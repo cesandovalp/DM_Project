5f071a7b57e05267a04413556d4ae79c8805833f
cesandovalp <cesandovalp@unal.edu.co>
2016-03-07 16:03:03 -0500

    Script updated for documentd downloads

--- script.R
+++ script.R
@@ -1,14 +1,16 @@
-#setwd("~/Documents/MDA/DM_Project/")
 library(stringr)
+library(tm)
+
+setwd("~/Documents/MDA/DM_Project/")
 
 download.file("http://arxiv.org/corr/home", destfile = "home.html")
 home.html = readLines("home.html")
 p1 = regexpr('http.*arxiv.*cs.*recent', home.html, TRUE)
-p2 = p1 + attr(p1, "match.length") - 1
+p2 = p1 + attr(p1, "match.length") - 7
 str_match = substr(home.html, p1, p2)
 
-categories =list()
-counter = 0
+categories = list()
+counter = 1
 
 for(i in 1:length(str_match))
 {
@@ -19,26 +21,37 @@ for(i in 1:length(str_match))
   }
 }
 
-#for(i in 1:length(categories))
-#{
-#  print(categories[[i]])
-#}
-
 p1 = regexpr('cs...', categories, TRUE)
 p2 = p1 + attr(p1, "match.length") - 1
 categories.names = substr(categories, p1, p2)
 
+counter = 1
+pdf.files = list()
+
 for(i in 1:length(categories))
 {
-  categories.names[i] = str_replace_all(string=paste("./", categories.names[i]), pattern=" ", repl="")
+  categories.names[i] = paste("./", categories.names[i], sep="")
   dir.create(categories.names[i])
   setwd(categories.names[i])
-  #print(getwd())
-  download.file(categories[[i]], destfile = "file.html")
+  download.file(paste(categories[[i]], "1506", "?show=20", sep=""), destfile = "file.html")
   file.html = readLines("file.html")
-  p1 = regexpr('.pdf.*', home.html, TRUE)
+  p1 = regexpr('.pdf/[0-9]*.[0-9]*', file.html, TRUE, )
   p2 = p1 + attr(p1, "match.length") - 1
-  str_match = substr(home.html, p1, p2)
-  print(str_match)
+  str_match2 = substr(file.html, p1, p2)
+  
+  for(j in 1:length(str_match2))
+  {
+    if(str_match2[j] != "")
+    {
+      pdf.files[counter] = paste("http://arxiv.org", str_match2[[j]], sep="")
+      download.file(pdf.files[[counter]], destfile = paste(as.character(counter), ".pdf", sep = ""))
+      counter = counter + 1
+    }
+  }
   setwd("../")
 }
+
+tt <- readPDF()
+rr <- tt(elem=list(uri="cs.AI/10.pdf"),language="en",id="id1")
+rr[1:15]
+rr$content[2]
