edd3e14cfad999b4848ede723e06cfee83e78927
cesandovalp <cesandovalp@unal.edu.co>
2016-03-08 12:37:04 -0500

    TFIDF and cosine distance added

--- script.R
+++ script.R
@@ -1,5 +1,7 @@
 library(stringr)
 library(tm)
+library(wordcloud)
+library(proxy)
 
 setwd("~/Documents/MDA/DM_Project/")
 
@@ -27,6 +29,7 @@ categories.names = substr(categories, p1, p2)
 
 counter = 1
 pdf.files = list()
+pdf.index = list()
 
 for(i in 1:length(categories))
 {
@@ -44,6 +47,8 @@ for(i in 1:length(categories))
     if(str_match2[j] != "")
     {
       pdf.files[counter] = paste("http://arxiv.org", str_match2[[j]], sep="")
+      pdf.index[counter] = paste(getwd(), "/", as.character(counter), ".pdf", sep = "")
+      pdf.index[100:150]
       download.file(pdf.files[[counter]], destfile = paste(as.character(counter), ".pdf", sep = ""))
       counter = counter + 1
     }
@@ -52,6 +57,49 @@ for(i in 1:length(categories))
 }
 
 tt <- readPDF()
-rr <- tt(elem=list(uri="cs.AI/10.pdf"),language="en",id="id1")
-rr[1:15]
-rr$content[2]
+rr = list()
+rr2 = list()
+
+for(i in 1:length(pdf.index))
+{
+  rr[i] <- tt(elem=list(uri=pdf.index[[i]]),language="en",id="id1")
+}
+
+for(i in 1:length(pdf.index))
+{
+  rr2[i] = rr[[i]]
+  review_text = paste(rr[[i]], collapse = " ")
+  review_text_vector = unlist(strsplit(review_text, split=" "))
+  index.bad = grep("review_text_vector", iconv(review_text_vector, "latin1", "ASCII", sub="review_text_vector"))
+  review_subset = review_text_vector[-index.bad]
+  rr2[i] = paste(review_subset, collapse = " ")
+}
+
+review_src = VectorSource(rr2)
+corpus = Corpus(review_src)
+corpus = tm_map(corpus, content_transformer(tolower))
+corpus = tm_map(corpus, removePunctuation)
+corpus = tm_map(corpus, stripWhitespace)
+corpus = tm_map(corpus, removeWords, stopwords("english"))
+corpus = tm_map(corpus, removeNumbers)
+corpus = tm_map(corpus, stemDocument)
+
+#matriz_dt = DocumentTermMatrix(corpus, control = list(weighting = weightTfIdf))
+matriz_dt2 = DocumentTermMatrix(corpus)
+
+dtm2 = as.matrix(matriz_dt)
+dtm22 = as.matrix(matriz_dt2)
+freq = colSums(dtm2)
+freq2 = colSums(dtm22)
+freq = sort(freq, decreasing = TRUE)
+freq2 = sort(freq2, decreasing = TRUE)
+head(freq)
+head(freq2)
+barplot(freq)
+barplot(freq2)
+
+words = names(freq)
+wordcloud(words[1:100], freq[1:100])
+
+result.cosine = proxy::dist(dtm2, method = "cosine")
+result.cosine[1:length(result.cosine)]
