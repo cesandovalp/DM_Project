60c701ef0c4286178ff279fc4dabd92335e4f3bf
cesandovalp <cesandovalp@unal.edu.co>
2016-05-22 06:18:56 -0500

    Updated

--- TF_IDF/TFIDF/R/clustering.R
+++ TF_IDF/TFIDF/R/clustering.R
@@ -5,8 +5,12 @@ test_pre = function()
   sw_path = system.file("extdata", "list_files.txt", package = "TFIDF")
   documents = readLines(sw_path)
 
+  result = list()
+
   for( i in documents )
   {
-    txt.table(i)
+    result[i] = txt.table(i)
   }
+
+  result
 }
